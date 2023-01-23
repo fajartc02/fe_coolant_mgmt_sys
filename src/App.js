98dc82c54486c11deb2eb10278b92ba14c53c160
helmiyogantara <helmiyogantara@gmail.com>
2023-01-24 02:52:30 +0700

    setup redux

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component, Suspense } from 'react'
-import { HashRouter, Route, Routes } from 'react-router-dom'
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom'
+import PrivateRoutes from './utils/PrivateRoutes'
 import './scss/style.scss'
 
 const loading = (
@@ -20,17 +21,19 @@ const Page500 = React.lazy(() => import('./views/pages/page500/Page500'))
 class App extends Component {
   render() {
     return (
-      <HashRouter>
+      <Router>
         <Suspense fallback={loading}>
           <Routes>
+            <Route element={<PrivateRoutes />}>
+              <Route path="*" name="Home" element={<DefaultLayout />} />
+            </Route>
             <Route exact path="/login" name="Login Page" element={<Login />} />
             <Route exact path="/register" name="Register Page" element={<Register />} />
             <Route exact path="/404" name="Page 404" element={<Page404 />} />
             <Route exact path="/500" name="Page 500" element={<Page500 />} />
-            <Route path="*" name="Home" element={<DefaultLayout />} />
           </Routes>
         </Suspense>
-      </HashRouter>
+      </Router>
     )
   }
 }
